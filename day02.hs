3ee50d1e59cadc0810c7ee74e80efebd47d3a5c8
Henri Lahtinen <henri.lahtinen@sok.fi>
2023-12-02 22:00:28 +0200

    day02: second part

--- day02.hs
+++ day02.hs
@@ -6,12 +6,16 @@ import Data.List.Split (splitOn)
 data Game = Game Int [Cubes] deriving (Show)
 type Cubes = Map String Int
 
-main = interact (unlines . sequence [part1] . map parse . lines)
+main = interact (unlines . sequence [part1, part2] . map parse . lines)
 
 part1 :: [Game] -> String
 part1 = ("Part 1: " ++) . show . sum . map gid . filter (possible bag)
 -- only 12 red cubes, 13 green cubes, and 14 blue cubes
   where bag = Map.fromList [("red", 12), ("green", 13), ("blue", 14)]
+
+part2 = ("Part 2: " ++) . show . sum . map (product . fewest)
+  where
+    fewest (Game _ hands) = Map.unionsWith max hands
 
 possible :: Cubes -> Game -> Bool
 possible bag (Game n hands) = all (all (>= 0) . Map.unionWith (+) bag . Map.map negate) hands
