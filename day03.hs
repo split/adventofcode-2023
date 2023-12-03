6be68beb52bb3ec5a78cfb57b6a516f74220d42f
Henri Lahtinen <henri.lahtinen@sok.fi>
2023-12-03 15:51:14 +0200

    day03: dirty solutions for both parts

---
+++ day03.hs
@@ -0,0 +1,50 @@
+module Main where
+
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Char (isDigit)
+import Data.Maybe (mapMaybe, listToMaybe)
+import Data.List (find, nub)
+
+data Point = Part String (Int, Int) | Symbol Char deriving (Show, Eq)
+data Schematic = Map (Int, Int) Point
+
+main = interact (unlines . sequence [part1, part2] . lines)
+
+part1 = ("Part 1: " ++ ) . show . sum . nub . map countSymbols . findAdjacent
+  where
+    countSymbols (Part pid _, adj) = if any isSymbol adj then read pid else 0
+    countSymbols _ = 0
+
+part2 = ("Part 2: " ++ ) . show . sum . map gearRatio . findAdjacent
+  where
+    gearRatio (Symbol '*', [Part p1 _, Part p2 _]) = read p1 * read p2
+    gearRatio _ = 0
+
+findAdjacent input = Map.elems $ Map.mapWithKey solve schematic
+  where
+    schematic = toSchematic input
+    solve xy point = (point, nub $ mapMaybe (schematic Map.!?) (adjacents xy len))
+      where
+        len = case point of 
+          Part pid _ -> length pid
+          _ -> 1
+
+adjacents :: (Int, Int) -> Int -> [(Int, Int)]
+adjacents (x, y) len = [(x+dx, y+dy) | dx <- [(-1)..len], dy <- [-1..1], not (dy == 0 && (dx >= 0 && dx < len))]
+
+toSchematic input = Map.fromList [((x, y), c) | (y, row) <- zip [0..] input, (x, c) <- toPoint row (0, y)]
+
+toPoint :: String -> (Int, Int) -> [(Int, Point)]
+toPoint [] _ = []
+toPoint str@(c:_) (x, y) = zip [x..] points ++ toPoint (drop len str) (x + len, y)
+  where
+    len = max (length points) 1
+    points
+      | isDigit c = let pid = takeWhile isDigit str in map (const (Part pid (x, y))) pid
+      | c == '.'  = []
+      | otherwise = [Symbol c]
+
+isSymbol x = case x of 
+  (Symbol _) -> True
+  _ -> False
