7c8959866a31e08fbd12e1975b3eb15a3c79d193
Henri Lahtinen <henri.lahtinen@sok.fi>
2023-12-04 14:53:02 +0200

    day04: second part

--- day04.hs
+++ day04.hs
@@ -1,9 +1,16 @@
 module Main where
 
 import Data.List (group, sort)
+import Data.Bifunctor (first, second)
 
-main = interact (unlines . sequence [part1] . map parse . lines)
+main = interact (unlines . sequence [part1, part2] . map parse . lines)
 
 part1 = ("Part 1: " ++) . show . sum . map ((2^) . pred . length) . filter (not . null)
+
+part2 = ("Part 2: " ++) . show  . sum . map fst . copies . map ((1,) . length)
+
+copies :: [(Int, Int)] -> [(Int, Int)]
+copies  (card@(c, n) : xs) = card : copies (map (first (+ c)) (take n xs) ++ drop n xs)
+copies _ = []
 
 parse = filter ((>1) . length). group . sort . words
