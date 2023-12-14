97c5458e5a1a796d214566207087e34d83233f4d
Henri Lahtinen <henri.lahtinen@sok.fi>
2023-12-14 20:56:17 +0200

    day13: second part

--- day13.hs
+++ day13.hs
@@ -3,21 +3,25 @@ module Main where
 import Control.Applicative ((<|>))
 import Control.Arrow ((&&&))
 import Control.Monad (ap)
-import Data.List (inits, tails, transpose)
+import Data.List (group, inits, sort, tails, transpose)
 import Data.List.Split (splitOn)
 import Data.Maybe (mapMaybe)
 
-main = interact (unlines . sequence [part1] . map lines . splitOn "\n\n")
+main = interact (unlines . sequence [part1, part2] . map lines . splitOn "\n\n")
 
-part1 = ("Part 1: " ++) . show . sum . mapMaybe summarize
+part1, part2 :: [[String]] -> String
+part1 = ("Part 1: " ++) . show . sum . mapMaybe (summarize and)
+part2 = ("Part 2: " ++) . show . sum . mapMaybe (summarize smudge)
 
-summarize mirror = mirrorReflection (transpose mirror) <|> ((* 100) <$> mirrorReflection mirror)
+summarize rule mirror = mirrorReflection rule (transpose mirror) <|> ((* 100) <$> mirrorReflection rule mirror)
 
-mirrorReflection :: (Eq a) => [[a]] -> Maybe Int
-mirrorReflection = go 1 []
+smudge = (== 1) . length . filter not
+
+mirrorReflection :: (Eq a) => ([Bool] -> Bool) -> [[a]] -> Maybe Int
+mirrorReflection rule = go 1 []
   where
     go _ _ [] = Nothing
     go i refl (x : xs)
       | null xs = Nothing
-      | and $ zipWith (==) (x : refl) xs = return i
+      | rule $ zipWith (==) (concat (x : refl)) (concat xs) = return i
       | otherwise = go (i + 1) (x : refl) xs
