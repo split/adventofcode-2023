9fb330ebdc0c47cc77b9ca645add6931eae32318
Henri Lahtinen <henri.lahtinen@sok.fi>
2023-12-14 18:15:49 +0200

    day14: second part

--- day14.hs
+++ day14.hs
@@ -1,19 +1,44 @@
 module Main where
 
-import Control.Monad (ap)
 import Data.List (intercalate, partition, transpose)
 import Data.List.Split (splitOn)
+import Data.Set qualified as Set
 import Data.Tuple (swap)
-import Debug.Trace (trace)
 
-main = interact (unlines . sequence [part1] . lines)
+data Tilt = N | E | S | W deriving (Ord, Eq, Enum, Show)
 
-part1 = ("Part 1: " ++) . show . load . map gravity . (!! 3) . rotations
+main = interact (unlines . sequence [part1, part2] . lines)
 
-gravity :: String -> String
-gravity = intercalate "#" . map (uncurry (++) . swap . partition (== '.')) . splitOn "#"
+part1, part2 :: [String] -> String
+part1 = ("Part 1: " ++) . show . load . gravity N
+part2 =
+  ("Part 2: " ++)
+    . maybe "Cycle not found" (show . load)
+    . spinCycle 1000000000
+    . iterate (flip (foldl (flip gravity)) [N .. W])
+
+gravity :: Tilt -> [String] -> [String]
+gravity tilt = vt . map (intercalate "#" . map (uncurry (++) . ht . partition (== '.')) . splitOn "#") . vt
+  where
+    (vt, ht) = case tilt of
+      N -> (transpose, swap)
+      W -> (id, id)
+      S -> (transpose, id)
+      E -> (id, swap)
+
+spinCycle :: Int -> [[String]] -> Maybe [String]
+spinCycle target platforms = do
+  start <- findDuplicate platforms
+  repeating <- findDuplicate (drop start platforms)
+  return $ platforms !! (start + (target - start) `mod` repeating)
 
 load :: [String] -> Int
-load = sum . concatMap (zipWith (\l b -> if b == 'O' then l else 0) [1 ..] . reverse)
+load = sum . concatMap (zipWith (\l b -> if b == 'O' then l else 0) [1 ..] . reverse) . transpose
 
-rotations = iterate transpose
+findDuplicate :: (Eq a, Ord a) => [a] -> Maybe Int
+findDuplicate = go Set.empty
+  where
+    go _ [] = Nothing
+    go seen (x : xs)
+      | x `Set.member` seen = Just (Set.size seen)
+      | otherwise = go (Set.insert x seen) xs
