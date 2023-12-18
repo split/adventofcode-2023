1d36431c16e8e1f6cc465cf9a7a80b3d980369f0
Henri Lahtinen <henri.lahtinen@sok.fi>
2023-12-18 11:26:07 +0200

    day18: solutions

---
+++ day18.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE LambdaCase #-}
+
+module Main where
+
+import Control.Monad (ap)
+import Data.List (group)
+
+data Dir = R | D | L | U deriving (Show, Eq, Enum, Read, Bounded)
+
+type Move = (Dir, Int)
+
+main = interact (unlines . sequence [part1, part2] . map words . lines)
+
+part1, part2 :: [[String]] -> String
+part1 = ("Part 1: " ++) . show . calc . map (follow . readMove)
+part2 = ("Part 2: " ++) . show . calc . map (follow . readHex . (!! 2))
+
+calc = ap ((+) . border) (area . scanl move (0, 0))
+  where
+    area xs = (`div` 2) . abs . sum $ zipWith (\(x1, y1) (x2, y2) -> (y1 + y2) * (x2 - x1)) xs (tail (cycle xs))
+    border = (+ 1) . (`div` 2) . sum . map (\(x, y) -> abs x + abs y)
+
+follow :: Move -> (Int, Int)
+follow = \case
+  (R, n) -> (n, 0)
+  (L, n) -> (-n, 0)
+  (D, n) -> (0, n)
+  (U, n) -> (0, -n)
+
+readMove :: [String] -> Move
+readMove (d : n : _) = (read d, read n)
+
+readHex :: String -> Move
+readHex xs = (toEnum (read [xs !! 7]), read ("0x" ++ take 5 (drop 2 xs)))
+
+move (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
